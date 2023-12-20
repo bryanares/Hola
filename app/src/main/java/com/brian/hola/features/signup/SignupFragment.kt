f42f9e8934f19cafcfb8e08eeb5cd2eeeeba6b38
bryanares <bryanares12@gmail.com>
2023-12-20 10:57:12 +0300

    features screens

---
+++ app/src/main/java/com/brian/hola/features/signup/SignupFragment.kt
@@ -0,0 +1,40 @@
+package com.brian.hola.features.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.brian.hola.R
+import com.brian.hola.databinding.FragmentSignupBinding
+
+class SignupFragment : Fragment() {
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.SignupBtn.setOnClickListener {
+
+            findNavController().navigate(R.id.action_signupFragment_to_homeFragment)
+        }
+
+        binding.loginLinkTxt.setOnClickListener {
+
+            findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+        }
+        return view
+    }
+
+}
