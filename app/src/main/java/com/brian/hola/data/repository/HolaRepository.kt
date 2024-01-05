9c9c3171ffe892e680d7dab08760f115f4a2edb2
bryanares <bryanares12@gmail.com>
2024-01-05 12:06:00 +0300

    describe repository methods

--- app/src/main/java/com/brian/hola/data/repository/HolaRepository.kt
+++ app/src/main/java/com/brian/hola/data/repository/HolaRepository.kt
@@ -1,4 +1,13 @@
 package com.brian.hola.data.repository
 
+import com.brian.hola.data.model.User
+import com.brian.hola.data.utils.Rezults
+import kotlinx.coroutines.flow.Flow
+
 interface HolaRepository {
+    suspend fun login(userEmail: String, userPassword: String): Flow<Rezults<User>>
+
+    suspend fun signUp(userEmail: String, userPassword: String, userName: String) : Flow<Rezults<User>>
+
+
 }
