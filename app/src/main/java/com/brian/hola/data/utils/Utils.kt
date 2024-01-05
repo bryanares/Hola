801482623fb52e86717fdac94b80804611e65af1
bryanares <bryanares12@gmail.com>
2024-01-05 12:35:58 +0300

    describe Results

--- app/src/main/java/com/brian/hola/data/utils/Utils.kt
+++ app/src/main/java/com/brian/hola/data/utils/Utils.kt
@@ -1,2 +1,12 @@
 package com.brian.hola.data.utils
 
+import java.lang.Exception
+
+sealed class Rezults<T> {
+
+    data class Success<T>(val data: T) : Rezults<T>()
+
+    data class Error<T>(val message: String? = null, val exception: Exception? = null) :
+        Rezults<T>()
+
+}
