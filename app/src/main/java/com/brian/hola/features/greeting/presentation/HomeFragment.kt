09bd57cffe013f4dd27e4b3687174e82f80938b9
bryanares <bryanares12@gmail.com>
2024-01-05 12:28:28 +0300

    viewbinding

--- app/src/main/java/com/brian/hola/features/greeting/presentation/HomeFragment.kt
+++ app/src/main/java/com/brian/hola/features/greeting/presentation/HomeFragment.kt
@@ -1,25 +1,33 @@
-package com.brian.hola.presentation
+package com.brian.hola.features.greeting.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.brian.hola.R
+import androidx.fragment.app.viewModels
+import com.brian.hola.data.repository.HolaRepository
+import com.brian.hola.databinding.FragmentHomeBinding
+import com.brian.hola.features.auth.domain.viewModel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+
     }
 
 }
