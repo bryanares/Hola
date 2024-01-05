9fd0d639bdf16fe8be14193e3f341bc463c0bcee
bryanares <bryanares12@gmail.com>
2024-01-05 12:27:23 +0300

    log in user and navigate to HomeFragment

--- app/src/main/java/com/brian/hola/features/auth/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/brian/hola/features/auth/presentation/login/LoginFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.brian.hola.R
 import com.brian.hola.databinding.FragmentLoginBinding
+import com.brian.hola.features.auth.domain.viewModel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val authViewModel: AuthViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,16 +31,45 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.loginBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-        }
+        return view
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectStates()
+        binding.loginBtn.setOnClickListener {
+            authViewModel.login(
+                binding.loginEmail.text.toString(),
+                binding.loginPassword.text.toString()
+            )
+        }
         binding.signupLinkTxt.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
         }
+    }
 
-        return view
+    private fun collectStates() {
+        viewLifecycleOwner.lifecycleScope.launch {
 
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                authViewModel.authUiState.collectLatest { state ->
+                    binding.loginBtn.isEnabled = !state.isLoading
+                    authViewModel.resetState()
+                    if (state.isSuccessful) {
+                        findNavController().navigate(
+                            LoginFragmentDirections.actionLoginFragmentToHomeFragment(
+                                state.userId!!
+                            )
+                        )
+                    }
+                    if (state.error != null) {
+                        Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT).show()
+                    }
+                    authViewModel.resetState()
+                }
+            }
+        }
     }
 
 }
