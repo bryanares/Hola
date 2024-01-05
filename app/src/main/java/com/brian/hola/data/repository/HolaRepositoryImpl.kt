ec1559eba9784a87bd7b2b86b4acdc9831f8ba41
bryanares <bryanares12@gmail.com>
2024-01-05 12:06:30 +0300

    implement repository methods

--- app/src/main/java/com/brian/hola/data/repository/HolaRepositoryImpl.kt
+++ app/src/main/java/com/brian/hola/data/repository/HolaRepositoryImpl.kt
@@ -1,4 +1,69 @@
 package com.brian.hola.data.repository
 
-class HolaRepositoryImpl {
+import com.brian.hola.data.model.User
+import com.brian.hola.data.utils.Rezults
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+class HolaRepositoryImpl @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : HolaRepository {
+    override suspend fun login(userEmail: String, userPassword: String): Flow<Rezults<User>> {
+
+        return callbackFlow {
+            firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        this.trySend(
+                            Rezults.Success(
+                                User(
+                                    firebaseAuth.currentUser!!.uid
+                                )
+                            )
+                        ).isSuccess
+                    } else {
+                        this.trySend(
+                            Rezults.Error(
+                                "",
+                                task.exception!!
+                            )
+                        ).isSuccess
+                    }
+                }
+            awaitClose { this.cancel() }
+        }
+    }
+
+
+    override suspend fun signUp(
+        userEmail: String,
+        userPassword: String,
+        userName: String
+    ): Flow<Rezults<User>> = callbackFlow {
+        firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+                    this.trySend(
+                        Rezults.Success(
+                            User(
+                                firebaseAuth.currentUser!!.uid
+                            )
+                        )
+                    ).isSuccess
+                } else {
+                    this.trySend(
+                        Rezults.Error(
+                            "",
+                            task.exception!!
+                        )
+                    ).isSuccess
+                }
+            }
+        awaitClose { this.cancel() }
+    }
 }
